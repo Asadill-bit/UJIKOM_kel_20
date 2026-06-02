a6eb76d5f6bfb9ae0b29f87db87bd80cc592c495
Syakir <syakirfajarrahayu@gmail.com>
2026-06-02 09:17:20 +0700

    ci:merubah config firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -8,14 +8,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzBDdHWLMkWN7bH3oJBdri6s5KRBM6EHk",
-  authDomain: "monitoring-iot-29ac6.firebaseapp.com",
-  databaseURL: "https://monitoring-iot-29ac6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "monitoring-iot-29ac6",
-  storageBucket: "monitoring-iot-29ac6.firebasestorage.app",
-  messagingSenderId: "4724063661",
-  appId: "1:4724063661:web:0a03e34b61072f9fd97524",
-  measurementId: "G-2Y44SR9LMN"
+  apiKey: "AIzaSyBQJ7KjqcLmzUmth5vIQupVQLfnP_rbUCk",
+  authDomain: "monitoring-iot-ujikom-ea02d.firebaseapp.com",
+  databaseURL: "https://monitoring-iot-ujikom-ea02d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "monitoring-iot-ujikom-ea02d",
+  storageBucket: "monitoring-iot-ujikom-ea02d.firebasestorage.app",
+  messagingSenderId: "12076528888",
+  appId: "1:12076528888:web:ca726664ddf7b00ae1b70d",
+  measurementId: "G-WL5HPXL6XH"
 };
 
 const app = initializeApp(firebaseConfig);
